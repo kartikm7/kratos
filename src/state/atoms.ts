8dffb63106dce368e52bb52ff01004173af31734
Kartikeya Mishra <kartikeyavkm@gmail.com>
2026-06-14 01:15:42 +0530

    feat: fs tools (read, write and edit)
    - had to move away from bash-tool for this, since these actually edit
    the file

--- src/state/atoms.ts
+++ src/state/atoms.ts
@@ -5,7 +5,13 @@ import {
   type Model,
   type ModelsList,
 } from "./types";
-import { SandboxBashTools, WebBrowserTool } from "../utils/tools";
+import {
+  ReadFile,
+  EditFile,
+  WriteFile,
+  SandboxBashTools,
+  WebBrowserTool,
+} from "../utils/tools";
 
 export const llmAtom = atom<any>(); // need to figure a generic type for this
 // TODO: Most likely string is not the right type, when I start adding tools this will most likely cause a problem
@@ -15,5 +21,11 @@ export const modelsListAtom = atom<ModelsList | null>(null);
 export const connectedProvidersAtom = atom<ConnectedProvidersList>();
 
 // for whatever reason it's not possible to load this in a hook so this is what we gotta do!
-const sandBoxBashTools = await SandboxBashTools();
-export const toolsAtom = atom({ ...sandBoxBashTools, WebBrowserTool });
+const sandBoxBashTool = await SandboxBashTools();
+export const toolsAtom = atom({
+  sandBoxBashTool,
+  WebBrowserTool,
+  ReadFile,
+  WriteFile,
+  EditFile,
+});
--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -5,21 +5,71 @@ import { createBashTool } from "bash-tool";
 import puppeteer from "puppeteer-extra";
 import stealth from "puppeteer-extra-plugin-stealth";
 import TurndownService from "turndown";
+import { replaceInFile } from "replace-in-file";
+import { readFile, writeFile, exists } from "fs/promises";
 // import puppeteer from "puppeteer";
+
+// TODO: should add a buffered reader, since large codebases tend to have files with > 500 lines of code making this too large
+// could also be that the llm fallsback to sandboxed bash to read the files using grep and shit
+const ReadFile = tool({
+  description: "Read the contents of a file",
+  inputSchema: z.object({ path: z.string() }),
+  execute: async ({ path }) => {
+    try {
+      const contents = await readFile(path, { encoding: "utf-8" });
+      return contents;
+    } catch (error) {
+      return error;
+    }
+  },
+});
+
+const WriteFile = tool({
+  description: "Write a new file to a desired path",
+  inputSchema: z.object({ path: z.string(), contents: z.string() }),
+  execute: async ({ path, contents }) => {
+    try {
+      if (await exists(path))
+        throw new Error("A file of this name already exists");
+      await writeFile(path, contents);
+      return { success: true }; // how times fly, need to return a value that the llm needs to understand ffs
+    } catch (error) {
+      return error;
+    }
+  },
+});
+
+const EditFile = tool({
+  description: "Replace in file, replace exact first occurence",
+  inputSchema: z.object({
+    file: z.string(),
+    from: z.string(),
+    to: z.string(),
+  }),
+  execute: async ({ file, from, to }) => {
+    const options = { files: file, from, to };
+    try {
+      const results = await replaceInFile(options);
+      return results;
+    } catch (e) {
+      return String(e);
+    }
+  },
+});
 
 const SandboxBashTools = async () => {
   const overlayFs = new OverlayFs({
     root: process.cwd(),
     mountPoint: "/workspace",
   });
-  const bash = new Bash({
+  const sandboxBash = new Bash({
     fs: overlayFs,
     cwd: "/workspace",
   });
   const { tools } = await createBashTool({
-    sandbox: bash,
+    sandbox: sandboxBash,
     destination: "/workspace",
-    extraInstructions: `You have access to files and directories mounted at /workspace.
+    extraInstructions: `You have access to files and directories mounted at /workspace (Sandboxed Directory, Sandboxing is only for Bash Commands).
 Use bash commands to explore:
 - ls /workspace to see the directory structure
 - cat /workspace/filename to read files
@@ -30,7 +80,8 @@ Use bash commands to explore:
 Help the user explore, search, and understand the contents.`,
   });
 
-  return tools;
+  const { bash } = tools;
+  return bash;
 };
 
 // TODO: There's a lightweight browser, built just for this if we can migrate to that easily it will make life a whole lot better
@@ -57,4 +108,4 @@ const WebBrowserTool = tool({
   },
 });
 
-export { SandboxBashTools, WebBrowserTool };
+export { ReadFile, WriteFile, EditFile, SandboxBashTools, WebBrowserTool };

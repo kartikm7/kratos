67af3741b2d6c3fd2e3ec984a53b71fbc428a447
Kartikeya Mishra <kartikeyavkm@gmail.com>
2026-06-14 16:02:02 +0530

    fix: better-sqlite3 not working

--- src/utils/tools/knowledgeBase/memoryDb.ts
+++ src/utils/tools/knowledgeBase/memoryDb.ts
@@ -1,13 +1,14 @@
-import Database from "better-sqlite3";
-import { drizzle } from "drizzle-orm/better-sqlite3";
 import path from "path";
 import { AppDirectory } from "../../os";
 import fs from "fs";
+import Database from "libsql";
+import { drizzle } from "drizzle-orm/libsql";
 
 const location = path.join(AppDirectory, "knowledgebase.db");
 const init = async () => {
   if (exists()) return;
   const db = new Database(location);
+
   db.exec(` CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
@@ -20,10 +21,9 @@ const init = async () => {
 
 const query = async (query: string) => {
   if (!exists()) init();
-  const sqlite = new Database(location);
-  const db = drizzle({ client: sqlite });
-  const result = db.run(query);
-  return result;
+  const db = drizzle("file:" + location); // learned something new with the "file:" prefix
+  const result = await db.run(query);
+  return JSON.stringify(result);
 };
 
 const exists = () => {
